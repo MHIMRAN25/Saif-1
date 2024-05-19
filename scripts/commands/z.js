92d51ded1fe9f0a4ad24da258a1557e1428679ba
MHIMRAN25 <147571069+MHIMRAN25@users.noreply.github.com>
2024-05-19 17:42:29 +0600

    Update z.js

--- scripts/commands/z.js
+++ scripts/commands/z.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "saif",
   version: "0.0.2",
   permission: 0,
-  prefix: 'false',
+  prefix: false,
   credits: "Nayan",
   description: "talk with bot",
   category: "user",
